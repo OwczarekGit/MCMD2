42ef6fb9d8a5e1b46fc3812b8e30ce1e4ea742b4
OwczarekGit <32412218+OwczarekGit@users.noreply.github.com>
2023-07-07 18:34:14 +0200

    Replace with my api key.

--- src/curseforge/connection.rs
+++ src/curseforge/connection.rs
@@ -1,5 +1,5 @@
 pub static GAME_ID: &str = "432";
-pub static X_API_KEY: &str = "$2a$10$wuAJuNZuted3NORVmpgUC.m8sI.pv1tOPKZyBgLFGjxFp/br0lZCC";
+pub static X_API_KEY: &str = "$2a$10$iS4zDq9VOgVdN5qJFk2/pubNmJaMswUTecdhYzU9i7gd0rUr8UQgW";
 
 pub static API_URL_V1: &str = "https://api.curseforge.com/v1/";
 pub static API_URL_V2: &str = "https://api.curseforge.com/v2/";
